d533f51db229d0e164d624d8e89c2fecacbd15e1
Mohammad Mehedi Hasan <90519550+mehedi807@users.noreply.github.com>
2023-08-13 16:28:43 +0600

    Update lab_4.m

--- lab_4.m
+++ lab_4.m
@@ -13,3 +13,15 @@ while i<20
   i++;
 end
 fprintf('%f\n',c);
+
+
+f = @(x) x^3 - 0.165*x^2 + 3.993*10^-4;
+fp = @(x) 3*x^2 - 0.33*x;
+x0 = 0.05;
+i = 1;
+while i<5
+  x1 = x0 - (f(x0) / fp(x0) );
+  x0 = x1;
+  i++;
+endwhile
+fprintf('%f\n',x0);
